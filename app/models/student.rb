bf27c4b38f98623082f0830960eec4fdb2f3ab86
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-29 16:57:54 -0500

    Name and age complete

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,17 @@
 require_relative '../../db/config'
 
+class Student < ActiveRecord::Base
+
+  def name
+    first_name + " " + last_name
+  end
+
+  def age
+    now = Time.now.utc.to_date
+    now.year - birthday.year - (birthday.to_date.change(:year => now.year) > now ? 1 : 0)
+  end
+
+
+end
+
 # implement your Student model here
